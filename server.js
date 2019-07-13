46f89fcaca77588ebe18e2fc6ed99ae32a03fa69
Srinivas <srinivasrao683@gmail.com>
2019-07-13 18:09:08 +0530

    Added real text

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var fs = require('fs');
+
+app.use(express.static(__dirname + '/'));
+
+router.get("/",function(req,res){
+  res.sendFile("index.html");
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log(app.settings.env + ';__dirname:' + __dirname + ';');
+  console.log('ScheduleApp UI Server started @Port : ' + this.address().port);
+});
+
+//sudo node server.js
